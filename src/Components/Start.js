012ce065e069aa4144d64cda1ebbedde44adb2d4
felipe <pipe.castro.espinoza@gmail.com>
2021-02-27 21:23:12 -0600

    Version 1.0

--- src/Components/Start.js
+++ src/Components/Start.js
@@ -1,9 +1,21 @@
 import React from 'react'
-
+import fondo from '../Media/icono-start.jpg';
+import '../Styles/Start.css'
+import {Link} from 'react-router-dom'
 function Start(){
     return(
         <React.Fragment>
-            Start App-Pharmacy
+            
+        <div className="Fondo-Start">
+            <div className="icono-start">
+                <img src={fondo} alt=""/>
+            <div className="boton-start">
+                <Link to="/form">
+                    <button className="btn btn-success btn-lg">Click to view Pharmacys</button>
+                </Link>
+            </div>
+            </div>
+        </div>
         </React.Fragment>
     )
 }
